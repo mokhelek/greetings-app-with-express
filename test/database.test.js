3f55b7e51e8c0ddf6bb2e0627d18a5f6dfec177c
mokhelek <mkatleho191@gmail.com>
2023-07-31 16:51:30 +0200

    *

--- test/database.test.js
+++ test/database.test.js
@@ -1,108 +1,68 @@
-
-import assert from "assert" ;
+import assert from "assert";
 
 import db from "../public/js/db.js";
 
 import databaseInteraction from "../public/js/databaseLogic.js";
 
-const databaseInteractionInstance = databaseInteraction(db) ;
-
+const databaseInteractionInstance = databaseInteraction(db);
 
 describe("The basic database web app", function () {
-    this.timeout(6000)
+    this.timeout(6000);
 
     beforeEach(async function () {
-        try {
-            // clean the tables before each test run
-            await db.none("TRUNCATE TABLE greetingstest RESTART IDENTITY CASCADE;");
-          
-        } catch (err) {
-            console.log(err);
-            throw err;
-        }
+        await db.none("TRUNCATE TABLE greetingstest RESTART IDENTITY CASCADE;");
     });
 
     it("should able to add user ", async function () {
-        try {
+        await databaseInteractionInstance.addUser({ username: "Tom" });
 
-            await databaseInteractionInstance.addUser({"username": "Tom" });
-        
-            
-            let users = await databaseInteractionInstance.all();
+        let users = await databaseInteractionInstance.all();
 
-            assert.equal(1, users.length);
-          
-        } catch (err) {
-            console.log(err);
-        }
+        assert.equal(1, users.length);
     });
 
     it("get details of a specific user ", async function () {
-        try {
+        await databaseInteractionInstance.addUser({ username: "Bjorn" });
 
-            await databaseInteractionInstance.addUser({"username": "Bjorn" });
-            
-            let userData = await databaseInteractionInstance.getUserData("Bjorn");
-            let expectedOutput = {
-                counter :1,
-                username:'Bjorn'
-            }
-            assert.deepEqual(expectedOutput,userData );
-          
-        } catch (err) {
-            console.log(err);
-        }
+        let userData = await databaseInteractionInstance.getUserData("Bjorn");
+        let expectedOutput = {
+            counter: 1,
+            username: "Bjorn",
+        };
+        assert.deepEqual(expectedOutput, userData);
     });
-
 
     it("should be able to increment the user counter ", async function () {
-        try {
+        await databaseInteractionInstance.addUser({ username: "Bjorn" });
+        await databaseInteractionInstance.addUser({ username: "Bjorn" });
+        await databaseInteractionInstance.addUser({ username: "Bjorn" });
 
-            await databaseInteractionInstance.addUser({"username": "Bjorn" });
-            await databaseInteractionInstance.addUser({"username": "Bjorn" });
-            await databaseInteractionInstance.addUser({"username": "Bjorn" });
+        let userCount = await databaseInteractionInstance.getUserCount("Bjorn");
 
-            
-            let userCount = await databaseInteractionInstance.getUserCount("Bjorn");
-
-            assert.equal(3,userCount );
-          
-        } catch (err) {
-            console.log(err);
-        }
+        assert.equal(3, userCount);
     });
-
 
     it("Get a list of all greeted users", async function () {
-        try {
-            await databaseInteractionInstance.addUser({"username": "Tom" });
-            await databaseInteractionInstance.addUser({"username": "Tom" });
-            await databaseInteractionInstance.addUser({"username": "Kat" });
-            
-            let users = await databaseInteractionInstance.all();
-            let expectedOutput = [
-                {
-                  counter: 2,
-                  username: 'Tom'
-                },
-                {
-                  counter: 1,
-                  username: 'Kat'
-                }
-              ]
-              
-            assert.deepEqual(expectedOutput, users);
-        } catch (err) {
-            console.log(err);
-        }
-    });
+        await databaseInteractionInstance.addUser({ username: "Tom" });
+        await databaseInteractionInstance.addUser({ username: "Tom" });
+        await databaseInteractionInstance.addUser({ username: "Kat" });
 
+        let users = await databaseInteractionInstance.all();
+        let expectedOutput = [
+            {
+                counter: 2,
+                username: "Tom",
+            },
+            {
+                counter: 1,
+                username: "Kat",
+            },
+        ];
+
+        assert.deepEqual(expectedOutput, users);
+    });
 
     after(function () {
         db.$pool.end;
     });
-
-})
-
-
-
+});
